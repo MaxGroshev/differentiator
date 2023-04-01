6696c49c42403a76e94d19d25d3c8271b0701cd9
root <root@DESKTOP-KIBV616>
2023-04-01 14:04:05 +0300

    added files

---
+++ tree/tree.h
@@ -0,0 +1,79 @@
+#pragma once
+#define DEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include "./graph_lib/graphviz.h"
+#include "my_ASSERT.h"
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef int tree_data_type;
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+struct tree_node_t
+{
+    int            node_type;
+    tree_data_type value;
+    tree_node_t*   right;
+    tree_node_t*   left;
+};
+
+struct tree_t
+{
+    tree_node_t* root = NULL;
+    size_t       size = 0;
+    FILE*   html_logs;
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+enum NODE_TYPE
+{
+    TYPE_NUM = 1,
+    TYPE_OP  = 2,
+};
+
+enum OPER_TYPE
+{
+    OP_ADD = 43,
+    OP_SUB = 45,
+    OP_MUL = 42,
+    OP_DIV = 47,
+};
+
+enum TREE_CODE_OF_PRINT
+{
+    T_TREE_SUC_CREATED      =  0,
+    T_NODE_SUC_CREATED      =  1,
+    T_FAIL_OF_CREATING_NODE = -1,
+    T_L_EDGE_SUC_CREATED    =  2,
+    T_R_EDGE_SUC_CREATED    =  3,
+    T_FAIL_OF_CREATING_EDGE = -2,
+    T_TREE_PRINT            =  4,
+    T_NODE_SUC_DELETED      =  5,
+    T_TREE_WAS_CLEARED      =  6,
+    T_TYPE_NUM_HAS_CHILD    = -3,
+    T_DIVISION_BY_ZERO      = -4,
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+tree_t*      constructor (tree_t* pine);
+tree_node_t* tree_create (tree_t* pine, int node_type, tree_data_type value, tree_node_t* l_child = NULL, tree_node_t* r_child = NULL);
+
+tree_node_t* tree_link_l (tree_t* pine, tree_node_t* parent, tree_node_t* child);
+tree_node_t* tree_link_r (tree_t* pine, tree_node_t* parent, tree_node_t* child);
+tree_node_t* tree_remove (tree_t* pine, tree_node_t* node);
+tree_node_t* tree_search (tree_node_t* tree_root, tree_node_t* node);
+
+void         graph_dump      (tree_t* pine);
+tree_node_t* tree_print      (FILE* graphviz, dump_graph_t* graph_dump_set, tree_node_t* parent);
+void         write_tree_logs (int code_of_print, tree_t* pine = NULL, tree_node_t* node = NULL);
+
+tree_node_t* tree_delete (tree_t* pine, tree_node_t* tree_root);
+
+void         signal_handler (int signal);
---
+++ tree/tree_dump.cpp
@@ -0,0 +1,42 @@
+#include "tree.h"
+
+void graph_dump (tree_t* pine)
+{
+    FILE* graphviz       = fopen ("dump_info/tree_dump.dot",  "w");
+    MY_ASSERT (graphviz != NULL)
+    dump_graph_t graph_dump_set  = {};
+    graph_dump_set.info.size = pine->size;
+    graph_dump_set.info.head = &pine->root->value;
+    init_graph   (graphviz, &graph_dump_set);
+    tree_print   (graphviz, &graph_dump_set, pine->root);
+
+    run_graphviz (graphviz, pine->html_logs, &graph_dump_set);
+    fclose       (graphviz);
+}
+
+tree_node_t* tree_print (FILE* graphviz, dump_graph_t* graph_dump_set, tree_node_t* parent)
+{
+    if (parent->node_type == TYPE_NUM)
+    {
+        graph_dump_set->nodes->fillcolor = "#FF8C69";
+        graph_dump_set->nodes->label     = "TYPE_NUM";
+        make_int_node (graphviz, graph_dump_set, &parent->value, *graph_dump_set->nodes, &parent->right->value, &parent->left->value, parent->value);
+    }
+    else if (parent->node_type == TYPE_OP)
+    {
+        graph_dump_set->nodes->fillcolor = "#77DD77";
+        graph_dump_set->nodes->label     = "TYPE_OP";
+        make_char_node (graphviz, graph_dump_set, &parent->value, *graph_dump_set->nodes, &parent->right->value, &parent->left->value, parent->value);
+    }
+
+    if (parent->left != NULL)
+    {
+        make_edge  (graphviz, graph_dump_set, &parent->value, &parent->left->value, *graph_dump_set->edges);
+        tree_print (graphviz, graph_dump_set, parent->left);
+    }
+    if (parent->right != NULL)
+    {
+        make_edge  (graphviz, graph_dump_set, &parent->value, &parent->right->value, *graph_dump_set->edges);
+        tree_print (graphviz, graph_dump_set, parent->right);
+    }
+}
